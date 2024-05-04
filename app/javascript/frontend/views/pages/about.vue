6dc4cac1bca0bd3eb8366f691f76351dfe7646f3
Anastasiia29Sh <nastya.sharcko@yandex.ru>
2024-05-04 22:57:58 +0300

    adding about page

--- app/javascript/frontend/views/pages/about.vue
+++ app/javascript/frontend/views/pages/about.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>О нас</h1>
+  <h1 class="title-page">{{ $t("about.title_page") }}</h1>
+  <p class="text-s">{{ $t("about.text_1") }}</p>
+  <br />
+  <p class="text-s">{{ $t("about.text_2") }}</p>
+  <br />
+  <p class="text-s">{{ $t("about.text_3") }}</p>
 </template>
+
+<style scoped lang="sass">
+.title-page
+  margin-bottom: 25px
+</style>
